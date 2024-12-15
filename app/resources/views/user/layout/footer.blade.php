67ef00c84976784d593bd521f4fbc54ae78e5900
taitbt3124 <taitbt3124@gmail.com>
2024-12-15 20:40:11 +0700

    fix: fail layout in footer

--- app/resources/views/user/layout/footer.blade.php
+++ app/resources/views/user/layout/footer.blade.php
@@ -10,10 +10,10 @@
                     </a>
                 </div>
                 <div class="col-lg-6">
-                    <div class="position-relative mx-auto">
+                    {{-- <div class="position-relative mx-auto">
                         <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="number" placeholder="Your Email">
                         <button type="submit" class="btn btn-primary border-0 border-secondary py-3 px-4 position-absolute rounded-pill text-white" style="top: 0; right: 0;">Đăng ký ngay</button>
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="col-lg-3">
                     <div class="d-flex justify-content-end pt-3">
